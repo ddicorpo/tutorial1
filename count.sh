b8e25dd45882be3e2eb707adb41a04af5f4386da
Daniel <ddicorpo@gmail.com>
2016-01-24 14:27:57 -0500

    order output by decreasing frequency per file

--- count.sh
+++ count.sh
@@ -5,5 +5,5 @@
 for filename in data/*.txt
 do
 	echo $filename
-	grep -v Species $filename | cut -d , -f 2 | sort | uniq -c
+	grep -v Species $filename | cut -d , -f 2 | sort | uniq -c | sort -n -r
 done
